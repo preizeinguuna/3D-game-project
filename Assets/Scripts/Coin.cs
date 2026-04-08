968fc617dfd9af82186a83b8e61b3bb29eeab15b
Inguna Preize <preizeinguna@gmail.com>
2026-04-08 15:04:41 +0300

    Updated coin script

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,40 +1,40 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Coin : MonoBehaviour
 {
-    public float turnsSpeed = 90f;
 
-    private void OnTriggerEnter(Collider other) {
+    [SerializeField] float turnSpeed = 90f;
 
+    private void OnTriggerEnter(Collider other)
+    {
         if (other.gameObject.GetComponent<Obstacle>() != null)
-            {
-                Destroy(gameObject);
-                return;
+        {
+            Destroy(gameObject);
+            return;
         }
 
-    
-        //Check if the object we collided with is the player
-        if (other.gameObject.name == "Player")
+        // Check that the object we collided with is the player
+        if (other.gameObject.name != "Player")
         {
             return;
         }
 
-        //Add points to the player score 
+        // Add to the player's score
+        //GameManager.inst.IncrementScore();
 
-
-        //Destroy the coin  object
+        // Destroy this coin object
         Destroy(gameObject);
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        transform.Rotate(0, 0, turnsSpeed * Time.deltaTime);
+        transform.Rotate(0, 0, turnSpeed * Time.deltaTime);
     }
 }
